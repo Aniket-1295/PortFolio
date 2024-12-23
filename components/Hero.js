2e4c7200acaa3bf1039e905aec2b68a6a3792986
Aniket <aniketkamble1642002@gmail.com>
2024-12-23 20:38:40 +0530

    Hero Component Added

---
+++ components/Hero.js
@@ -0,0 +1,62 @@
+"use client";
+
+import React from "react";
+import classes from "./Hero.module.css";
+import { useTypewriter, Cursor } from "react-simple-typewriter";
+// import image from "../public/terminal.png";
+const Hero = () => {
+  const [text] = useTypewriter({
+    words: ["Frontend Developer", "Backend Developer", "Fullstack Developer"],
+    loop: {},
+    typeSpeed: 120,
+    deleteSpeed: 80,
+  });
+  return (
+    <div className={classes.hero}>
+      <div className={classes.leftHero}>
+        <div className={classes.upper}>
+          <img className={classes.blur1} src="/Background Blurs.webp" alt="" />
+          <h2 className={classes.intro}>Hi all. I am</h2>
+          <h1 className={classes.name}>Aniket kamble</h1>
+          <div className={classes.role}>
+            <div className={classes.fixedSymbol}>&#62;</div>
+            <div className={classes.typing}>
+              <div className={classes.typingText}>{text}</div>
+              <div className={classes.cursor}>
+                <Cursor cursorStyle="|" />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={classes.lower}>
+          <div className={classes.comment}>
+            // checkout my projects section to see my work
+          </div>
+          <div className={classes.comment}>
+            // you can also see it on my github page
+          </div>
+          <div className={classes.comment1}>// Find my profile on Github</div>
+          <div className={classes.gitlink}>
+            <div className={classes.const}>const</div>
+            <div className={classes.variable}>githubLink</div>
+            <div className={classes.equal}>=</div>
+            <div className={classes.link}>
+              https://
+              <div className={classes.br} />
+              <div className={classes.rest}>github.com/Aniket-1295</div>
+              <div className={classes.rest2}>github.com/Aniket-1295/PortFolio</div>
+              {/* <div className={classes.rest}>?tab=repositories</div> */}
+            </div>
+            ;
+          </div>
+        </div>
+      </div>
+      <div className={classes.rightHero}>
+        <img className={classes.terminal} src="/terminal.webp" alt="" />
+        <img className={classes.blur} src="/Background Blurs.webp" alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
